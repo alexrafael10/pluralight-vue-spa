e9db79b2690aa048c3b794a47017cd78af7ea151
Alex <alexrafael10@gmail.com>
2021-02-02 09:23:21 -0300

    environment setup -- hot-reloading

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,7 +1,12 @@
 const webpack = require("webpack");
-const clientConfig = require("./webpack.base.config");
+const clientConfig = require("./webpack.client.config");
 
 module.exports = function setupDevServer(app) {
+    clientConfig.entry.app = ["webpack-hot-middleware/client", clientConfig.entry.app];
+    clientConfig.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    );
     const clientCompiler = webpack(clientConfig);
     app.use(
         require("webpack-dev-middleware")(clientCompiler, {
--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 
 const app = new Vue({
-    data: {
-        hello: "hi there2"
-    }
+  data: {
+    hello: "hi there2",
+  },
+  template: `<div id="app">{{hello}}</div>`,
 });
 
 export { app };
